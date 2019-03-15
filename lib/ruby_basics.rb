9e771406e58c7fe789363fbc4171d9a97a299424
Erin Ponsonby <erinponsonby@gmail.com>
2019-03-15 14:11:03 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,25 +3,25 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-
+  name = value
 end
 
 def argue
-
+  return argue
 end
 
-def greeting
+def greeting(arg1, arg2)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
 def pizza_party
-
+  return "cheese"
 end
